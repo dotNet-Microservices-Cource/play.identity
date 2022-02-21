ffe7dbcfe4cb1b4ea0b6da0f44a2ca3b81862019
Kristian Ganchev <kris.ganchev91@gmail.com>
2022-02-21 17:47:33 +0200

    Added service bus support

--- src/Play.Identity.Service/Startup.cs
+++ src/Play.Identity.Service/Startup.cs
@@ -46,7 +46,7 @@ namespace Play.Identity.Service
             serviceSettings.ServiceName
         );
 
-      services.AddMassTransitWithRabbitMq(retryConfigurator => 
+      services.AddMassTransitWithMessageBroker(Configuration ,retryConfigurator => 
       {
         retryConfigurator.Interval(3, TimeSpan.FromSeconds(5));
         retryConfigurator.Ignore(typeof(UserUnknownException));
